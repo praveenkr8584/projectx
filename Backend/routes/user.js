59c629b547bfb27ad80cf1032d9c9cd254d83a03
Praveen kumar <munnakr1357@gmail.com>
2025-11-01 19:54:24 +0530

    bcryptjs installation2

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { authenticateToken, authorizeCustomer } = require('../middleware/auth');
 const upload = require('../middleware/upload');
 const User = require('../models/user');
